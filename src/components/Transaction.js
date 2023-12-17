5295b5ccdc6fe403d257b1afb8f95b42b49fe038
Jay <jaycagliyan@gmail.com>
2023-12-17 05:27:38 -0500

    final

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,11 +1,17 @@
-import React from "react";
-export const Transaction = ({transaction}) => {
-    const sign = transaction.amount <0 ? '-' : '+';
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
+
+export const Transaction = ({ transaction }) => {
+    const { deleteTransaction } = useContext(GlobalContext);
+    const sign = transaction.amount < 0 ? '-' : '+';
+
     return (
         <div>
-            <li className={transaction.amount<0 ? 'minus' : 'plus'}>
-                {/* You need to cahtch transaction here using props or destructring */}
-                {transaction.text}  <span>{sign}${Math.abs(transaction.amount)}</span><button className='delete-btn'>x</button></li>
+            <li className={transaction.amount < 0 ? 'minus' : 'plus'}>
+                {/* You need to catch transaction here using props or destructring, we used destructring */}
+                {transaction.text}  <span>{sign}${Math.abs(transaction.amount)}</span><button
+                onClick={() => deleteTransaction(transaction.id)} className='delete-btn'>X</button>
+                </li>
 
         </div>
     )
